e64c3aaa50273a82e05873fbf133edc576fc599c
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-05-09 12:45:05 +0200

    Added makeInstant helper in OrderClass

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,6 +1,10 @@
 package edu.iis.mto.time;
 
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 
 class OrderTest {
 
@@ -9,6 +13,10 @@ class OrderTest {
     @BeforeAll public void initialization() {
         order = new Order();
         order.addItem(new OrderItem());
+    }
+
+    public Instant makeInstant(int numberOfHours) {
+        return Instant.now().plus(Duration.standardHours(numberOfHours));
     }
 
 }
